e551028ab3e1bcd588dc7e055cefc4b5bd5a31ba
Asatbek Xalimjonov <asatbekxalimjonov2000@gmail.com>
2022-06-28 14:30:41 +0500

    bugs
    Took 2 minutes

--- src/main/java/com/itransition/courseproject/entity/collection/Item.java
+++ src/main/java/com/itransition/courseproject/entity/collection/Item.java
@@ -8,9 +8,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
